6630f55c6dabf4030a04350557145ac1e9fa35c5
ealbanca <ealbanca@byui.edu>
2026-04-02 21:55:54 -0600

    Updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cms-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cms';
+  title = 'Jobs Poster';
 
 }
