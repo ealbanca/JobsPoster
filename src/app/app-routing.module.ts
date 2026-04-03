cf461839a29cb3ef210f6f86a283e1fb7b571dad
ealbanca <ealbanca@byui.edu>
2026-04-02 22:03:19 -0600

    Updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CompaniesComponent } from './companies/companies.component';
+import { CompanyDetailComponent } from './companies/company-detail/company-detail.component';
+import { CompanyEditComponent } from './companies/company-edit/company-edit.component';
+import { CompanyListComponent} from './companies/company-list/company-list.component';
+import { CompanyStartComponent } from './companies/company-start/company-start.component';
+import { JobsComponent } from './jobs/jobs.component';
+
+
 const appRoutes: Routes = [
-    // { path: '', component: HomeComponent },
-    // Add your routes here
+    {path: '', redirectTo: '/companies', pathMatch: 'full'},
+    {path: 'companies', component: CompaniesComponent, children: [
+        {path: '', component: CompanyStartComponent},
+        {path: 'new', component: CompanyEditComponent},
+        {path: ':id', component: CompanyDetailComponent},
+        {path: ':id/edit', component: CompanyEditComponent}
+    ]
+    },
+    {path: 'jobs', component: JobsComponent}
 ];
 
 
