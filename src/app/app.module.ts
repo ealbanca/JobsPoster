c24bc3ab5149c44f2f5c20abcbe78ce4f812e500
ealbanca <ealbanca@byui.edu>
2026-04-02 20:48:29 -0600

    Created companies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { CompaniesComponent } from './companies/companies/companies.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CompaniesComponent,
   ],
   imports: [
     BrowserModule,
