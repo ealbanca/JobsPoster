b6c3e5fd520902d374fc8369fbf91287634e5c58
ealbanca <ealbanca@byui.edu>
2026-04-03 21:01:30 -0600

    Created Company schema

---
+++ server/models/company.js
@@ -0,0 +1,22 @@
+const { log } = require('@angular-devkit/build-angular/src/builders/ssr-dev-server');
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+    description: {
+    type: String,
+  },
+  logoUrl: {
+    type: String,
+  },
+    websiteUrl: {
+    type: String,
+  },
+  jobs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Job',
+  }],
+});
