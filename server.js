d23222c46ce988cba28bab7aef24244a427c457e
ealbanca <ealbanca@byui.edu>
2026-04-04 19:49:46 -0600

    Completed error event handling to connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./routes/app');
+const app = require('./server/routes/app');
 const debug = require('debug')('node-angular');
 const http = require('http');
 
@@ -13,3 +13,38 @@ const normalizePort = val => {
     }
     return false;
 };
+
+// Event listener for HTTP server "error" event.
+const onError = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Event listener for HTTP server "listening" event.
+const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
+server.listen(port);
