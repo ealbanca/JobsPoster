a7a98e640a52520e401a6800cb39c96bf9719223
ealbanca <ealbanca@byui.edu>
2026-04-04 19:46:27 -0600

    Implementing server.js

--- server.js
+++ server.js
@@ -1,81 +1,15 @@
-// Get dependencies
-var express = require('express');
-var path = require('path');
-var http = require('http');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const app = require('./routes/app');
+const debug = require('debug')('node-angular');
+const http = require('http');
 
-// import the routing file to handle the default (index) route
-var index = require('./server/routes/app');
-
-// establish a connection to the mongo database
-mongoose.connect('mongodb://localhost:27017/cms')
-  .then(() => {
-    console.log('Connected to database!');
-  })
-  .catch((err) => {
-    console.log('Connection failed: ' + err);
-  });
-
-
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ... 
-const messageRoutes = require('./server/routes/messages');
-const contactRoutes = require('./server/routes/contacts');
-const documentRoutes = require('./server/routes/documents');
-
-
-var app = express(); // create an instance of express
-
-// Tell express to use the following parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-
-app.use(logger('dev')); // Tell express to use the Morgan logger
-
-// Add support for CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
-
-// Tell express to use the specified director as the
-// root directory for your web site
-app.use(express.static(path.join(__dirname, 'dist/cms/browser')));
-
-// Tell express to map the default route ('/') to the index route
-app.use('/', index);
-
-// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
-app.use('/messages', messageRoutes);
-app.use('/contacts', contactRoutes);
-app.use('/documents', documentRoutes);
-
-// Tell express to map all other non-defined routes back to the index page (Express 4.x compatible)
-app.all('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
-});
-
-// Define the port address and tell express to use this port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-// Create HTTP server.
-const server = http.createServer(app);
-
-// Tell the server to start listening on the provided port
-server.listen(port, function() {
-  console.log('API running on localhost: ' + port)
-});
+// Normalize a port into a number, string, or false.
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
--- server/routes/app.js
+++ server/routes/app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Company = require('../models/company');
-const Job = require('../models/job');
+// Create Express app
 const app = express();
 
+// Connect to MongoDB
 mongoose.connect('mongodb+srv://ealbanca:Slcw7278@jobpostercluster.frhemon.mongodb.net/').then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Connection Failed', err);
 });
 
+// Middleware
 app.use(bodyParser.json());
 
 module.exports = app;
