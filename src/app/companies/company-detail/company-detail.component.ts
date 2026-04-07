9727e5235568e665ef5e6638c34a6f5ccc9f25c1
ealbanca <ealbanca@byui.edu>
2026-04-06 19:46:37 -0600

    Added edit and delete on company details

--- src/app/companies/company-detail/company-detail.component.ts
+++ src/app/companies/company-detail/company-detail.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Company } from '../company.model';
+import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'cms-company-detail',
   templateUrl: './company-detail.component.html',
   styleUrl: './company-detail.component.css'
 })
-export class CompanyDetailComponent {
+
+export class CompanyDetailComponent implements OnInit {
+  company: Company;
+  id: number;
+
+  constructor(private companyService: CompanyService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.company = this.companyService.getCompany(this.id);
+        }
+      );
+  }
+  
+  onEditCompany() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+
+  onDeleteCompany() {
+    this.companyService.deleteCompany(this.id);
+    this.router.navigate(['/companies']);
+  }
 
 }
