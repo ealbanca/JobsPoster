e2c872497a2e6df6c248562a8ff7fd7d8f72abb6
ealbanca <ealbanca@byui.edu>
2026-04-06 21:23:08 -0600

    Implemented joblist component

--- src/app/jobs-list/jobs-list.component.ts
+++ src/app/jobs-list/jobs-list.component.ts
@@ -2,16 +2,32 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import {Job} from '../shared/job.model';
+import { JobsListService } from './jobs-list.service';
 
 @Component({
   selector: 'cms-jobs',
   templateUrl: './jobs-list.component.html',
   styleUrls: ['./jobs-list.component.css']
 })
-export class JobsListComponent implements OnInit{
+export class JobsListComponent implements OnInit, OnDestroy {
+  jobs: Job[] = [];
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private jobsListService: JobsListService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.jobs = this.jobsListService.getJobs();
+    this.subscription = this.jobsListService.jobsChanged
+      .subscribe((jobs: Job[]) => {
+        this.jobs = jobs;
+      });
+  }
+
+  onEditItem(index: number) {
+    this.jobsListService.startedEditing.next(index);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
