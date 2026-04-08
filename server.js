0e8978b0da8df8c8845e031fabd76353f8a57e31
ealbanca <ealbanca@byui.edu>
2026-04-07 21:01:12 -0600

    Connected to database, added routes and initialize express app

--- server.js
+++ server.js
@@ -5,3 +5,23 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+// import the routing file to handle the default (index) route
+const index = require('./server/routes/app');
+
+//establish a connection to the MongoDB database
+mongoose.connect('mongodb+srv://ealbanca:Slcw7278@jobpostercluster.frhemon.mongodb.net/').then(() => {
+    console.log('Connected to MongoDB');
+}
+).catch((err) => {
+    console.error('Connection Failed', err);
+}
+);
+
+// Import API routes
+const companyRoutes = require('./server/routes/companies');
+const jobRoutes = require('./server/routes/jobs');
+
+//create an instance of express app
+
+const app = express();
