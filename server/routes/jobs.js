27996463763e9bebb399a9620674d51b6dfaafba
ealbanca <ealbanca@byui.edu>
2026-04-07 21:09:22 -0600

    Added Get all jobs

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
-
 const Job = require('../models/Job');
 
 // Get all jobs
-router.get('/', async (req, res) => {
-    Job.find().then((jobs) => {
-        res.status(200).json({
-            message: 'Jobs fetched successfully',
-            jobs: jobs
-        });
-    }).catch((err) => {
-        res.status(500).json({ error: 'Failed to fetch jobs' });
+router.get('/', (req, res, next) => {
+  Job.find()
+    .populate('group')
+    .then(jobs => {
+      res.status(200).json({
+        message: 'Jobs fetched successfully!',
+        jobs: jobs
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'An error occurred',
+        error: error
+      });
     });
 });
