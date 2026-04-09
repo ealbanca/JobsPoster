5fedd7b1ecd3792e36bf6ecccf918ee9e6d6b07c
ealbanca <ealbanca@byui.edu>
2026-04-09 11:46:00 -0600

    added Update company

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Company = require('../models/Company');
+const sequenceGenerator = require('./seqGenerator');
 
 // Get all companies
 router.get('/', (req, res, next) => {
@@ -37,5 +38,64 @@ router.get('/:id', (req, res, next) => {
       });
     });
 });
+
+//Create a new company
+router.post('/', (req, res, next) => {
+  const maxCompanyId = sequenceGenerator.nextId('companies');
+  const company = new Company({
+    id: maxCompanyId,
+    name: req.body.name,
+    description: req.body.description,
+    logoUrl: req.body.logoUrl,
+    websiteUrl: req.body.websiteUrl,
+    jobs: req.body.jobs
+  });
+
+  company.save()
+    .then(createdCompany => {
+      res.status(201).json({
+        message: 'Company added successfully',
+        company: createdCompany
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'An error occurred',
+        error: error
+      });
+    });
+});
+
+//update a company
+
+router.put('/:id', (req, res, next) => {
+  Company.findOne({ _id: req.params.id })
+    .then(company => {
+      company.name = req.body.name;
+      company.description = req.body.description;
+      company.logoUrl = req.body.logoUrl;
+      company.websiteUrl = req.body.websiteUrl;
+      company.jobs = req.body.jobs;
+      Company.updateOne({ _id: req.params.id }, company)
+        .then(result => {
+          res.status(204).json({
+            message: 'Company updated successfully'
+          });
+        })
+        .catch(error => {
+          res.status(500).json({
+            message: 'An error occurred',
+            error: error
+          });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Company not found',
+        error: error
+      });
+    });
+});
+
 
 module.exports = router;
