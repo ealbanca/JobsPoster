1a089710a8ddad2d343f8266b8f0abec60215ad4
ealbanca <ealbanca@byui.edu>
2026-04-08 18:11:47 -0600

    Updated companies component

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company } from './company.model';
+import { CompanyService } from './company.service';
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrl: './companies.component.css'
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
+  selectedCompany : Company;
 
-}
+  constructor(private companyService: CompanyService) { }
+
   
+  ngOnInit() {
+    this.companyService.companySelectedEvent.subscribe(
+      (company: Company) => {
+        this.selectedCompany = company;
+      }
+    );
+  }
+}
