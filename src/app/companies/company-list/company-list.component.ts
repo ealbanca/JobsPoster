f94ae067ad360e4b38ef6691becbfe297f9497a4
ealbanca <ealbanca@byui.edu>
2026-04-08 18:09:57 -0600

    Updated company list

--- src/app/companies/company-list/company-list.component.ts
+++ src/app/companies/company-list/company-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { Company } from '../company.model';
@@ -13,37 +12,29 @@ import { CompanyService } from '../company.service';
 export class CompanyListComponent implements OnInit, OnDestroy {
   companies: Company[] = [];
   private subscription: Subscription;
-  private companiesChangedSubscription: Subscription;
-  
-  constructor(private companyService: CompanyService,
-              private router: Router,
-              private route: ActivatedRoute) { }
+  private changeSub: Subscription;
+
+  constructor(private companyService: CompanyService) {}
 
   ngOnInit() {
-    this.subscription = this.companyService.getCompanies().subscribe((response: any) => {
-      this.companies = response.companies;
-    });
-    this.companiesChangedSubscription = this.companyService.companyChangedEvent.subscribe((companies: Company[]) => {
+    // Subscribe to changes in the company list
+    this.changeSub = this.companyService.companyListChangedEvent.subscribe((companies: Company[]) => {
       this.companies = companies;
     });
+    // Fetch the initial list from the backend
+    this.subscription = this.companyService.getCompanies().subscribe((response: any) => {
+      this.companies = response.companies ? response.companies : response;
+      this.companyService.companies = this.companies;
+      this.companyService.sortAndSend();
+    });
   }
-
-  onEditCompany() {
-    this.router.navigate(['edit'], { relativeTo: this.route });
-  }
-
-  onNewCompany() {
-    this.router.navigate(['new'], { relativeTo: this.route });
-  }
-
-
 
   ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
-    if (this.companiesChangedSubscription) {
-      this.companiesChangedSubscription.unsubscribe();
+    if (this.changeSub) {
+      this.changeSub.unsubscribe();
     }
   }
 }
