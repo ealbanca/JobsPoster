68ffd251d8141fa7392fb142c02e3de5234900af
ealbanca <ealbanca@byui.edu>
2026-04-08 18:13:31 -0600

    Updated company service

--- src/app/companies/company.service.ts
+++ src/app/companies/company.service.ts
@@ -1,22 +1,41 @@
-import {Injectable, EventEmitter} from '@angular/core';
-import { Subject } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, EventEmitter} from "@angular/core";
+import { Subject } from "rxjs";
+import{ HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Company } from './company.model';
-import {Job} from '../shared/job.model';
+import { Company } from "./company.model";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class CompanyService {
-    companyListChanged = new Subject<Company[]>();
+    companyListChangedEvent = new Subject<Company[]>();
     companySelectedEvent = new EventEmitter<Company>();
-    company: Company[] = [];
+    companies: Company[] = [];
     companyChangedEvent = new EventEmitter<Company[]>();
+    maxCompanyId: number;
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { 
+        this.companies = []; // Initialize with empty array
+        this.maxCompanyId = this.getMaxId();
+    }
 
-    // get a copy of the companies array to prevent external modification
+    /*storeCompanies() {
+        const companies = JSON.stringify(this.companies);
+        const headers = new HttpHeaders({'Content-Type': 'application/json'});
+        this.http.put(
+            'https://cms-project-fca75-default-rtdb.firebaseio.com/companies.json',
+            companies,
+            { headers: headers }
+        ).subscribe(
+            () => {
+                this.companyListChangedEvent.next(this.companies.slice());
+            },
+            (error) => {
+                console.error('Error storing companies to server:', error);
+            }
+        );
+    }*/
+
     getCompanies() {
         return this.http.get<Company[]>('http://localhost:3000/companies');
     }
@@ -25,43 +44,73 @@ export class CompanyService {
         return this.http.get<Company>(`http://localhost:3000/companies/${id}`);
     }
 
+    getMaxId(): number {
+        let maxId = 0;
+        this.companies.forEach(company => {
+            const currentId = parseInt(company.id);
+            if (currentId > maxId) {
+                maxId = currentId;
+            }
+        });
+        return maxId;
+        }
+
     addCompany(company: Company) {
-        return this.http.post<Company>('http://localhost:3000/companies', company);
+        if (!company) {
+            return;
+        }
+        company.id = '';
+        const headers = new HttpHeaders({'Content-Type': 'application/json'});
+        // add to database
+        this.http.post<{ message: string, company: Company }>(
+            'http://localhost:3000/companies',
+            company,
+            { headers: headers })
+            .subscribe(
+                (responseData) => {
+                    this.companies.push(responseData.company);
+                    this.sortAndSend();
+                }
+            );
     }
 
     updateCompany(originalCompany: Company, newCompany: Company) {
-        if (!originalCompany || !newCompany || originalCompany === undefined || newCompany === undefined) {
+        if (!originalCompany || !newCompany) {
             return;
         }
-        const pos = this.company.findIndex(c => c.id === originalCompany.id);
+        const pos = this.companies.findIndex(c => c.id === originalCompany.id);
         if (pos < 0) {
             return;
         }
         newCompany.id = originalCompany.id;
         const headers = new HttpHeaders({'Content-Type': 'application/json'});
-        this.http.put(`http://localhost:3000/companies/${originalCompany.id}`, newCompany, { headers: headers })
-        .subscribe(
-            (response) => {
-                this.company[pos] = newCompany;
-                this.companyChangedEvent.emit(this.company.slice());
-            }
-        );
+        this.http.put(
+            'http://localhost:3000/companies/' + originalCompany.id,
+            newCompany,{ headers: headers })
+            .subscribe(
+                (response: any) => {
+                    this.companies[pos] = newCompany;
+                    this.sortAndSend();
+                }
+            );
     }
 
     deleteCompany(company: Company) {
         if (!company || company === undefined) {
             return;
         }
-        const pos = this.company.findIndex(c => c.id === company.id);
-        if (pos < 0) {
-            return;
-        }
-        this.http.delete(`http://localhost:3000/companies/${company.id}`)
-        .subscribe(
-            (response) => {
-                this.company.splice(pos, 1);
-                this.companyChangedEvent.emit(this.company.slice());
-            }
-        );
+        this.http.delete('http://localhost:3000/companies/' + company.id)
+            .subscribe((response: any) => {
+                // After deletion, fetch the updated list from backend
+                this.getCompanies().subscribe((res: any) => {
+                    this.companies = res.companies ? res.companies : res;
+                    this.sortAndSend();
+                });
+            });
     }
+    public sortAndSend() {
+        this.companies.sort((a, b) => a.name.localeCompare(b.name));
+        this.companyListChangedEvent.next(this.companies.slice());
+    }
+
 }
