a4d96f7219641dd2d831da53a283ba664ab87485
ealbanca <ealbanca@byui.edu>
2026-04-09 21:15:27 -0600

    Updated job and company models

--- src/app/companies/company.model.ts
+++ src/app/companies/company.model.ts
@@ -1,20 +1,12 @@
 import { Job } from '../shared/job.model';   
 
 export class Company {
-    public _id?: string; // MongoDB ObjectId
-    public id: string;
-    public name: string;
-    public description: string;
-    public logoUrl: string;
-    public websiteUrl: string;
-    public jobs: Job[];
 
-    constructor(id: string, name: string, description: string, logoUrl: string, websiteUrl: string) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.logoUrl = logoUrl;
-        this.websiteUrl = websiteUrl;
-        this.jobs = [];
+    constructor(public id: string, 
+        public name: string, 
+        public description: string, 
+        public logoUrl: string, 
+        public websiteUrl: string,
+        public jobs: Job[] = []) {
     }
 }
--- src/app/shared/job.model.ts
+++ src/app/shared/job.model.ts
@@ -1,19 +1,10 @@
 export class Job {
-    public id: number;
-    public title: string;
-    public description: string;
-    public location: string;
-    public salary: number;
-    public type: string;
-    public companyId: number;
 
-    constructor(id: number, title: string, description: string, location: string, salary: number, type: string, companyId: number) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.location = location;
-        this.salary = salary;
-        this.type = type;
-        this.companyId = companyId;
-    }
+    constructor(public id: string,
+        public title: string,
+        public description: string,
+        public location: string,
+        public salary: number,
+        public type: string,
+        public companyId: string) {}
 }
